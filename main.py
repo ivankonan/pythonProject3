dfe4601ef702070147f7f8cd5d2916ba5aaea787
ivankonan <89031207+ivankonan@users.noreply.github.com>
2021-08-19 15:53:47 +0300

    ДЗ1. Задание1. Вариант4

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+# создаем списки времен года
 winter=[1,2,12]
 spring=[3,4,5]
 summer=[6,7,8]
 autumn=[9,10,11]
+# создаем массив списков
 season={'Зима': winter,
         'Весна': spring,
         'Лето': summer,
         'Осень': autumn}
-month=int(input('Введите номер месяца '))
-for key in season.keys():
-    if month in season [key]:
-        print(key)
+month=int(input('Введите номер месяца ')) # вводим номер месяца
+for key in season.keys(): # цикл по массиву времен года
+    if month in season [key]: # проверка наличия номера месяце в списке сезона
+        print(key) # вывод результата
